50673221ccda4e16b77f83a40689e824876ac932
Ongki Herlambang <meepomodeon@gmail.com>
2019-06-23 23:39:06 +0700

    add prismic as default stater CMS, setup the config and node with dummy CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,63 @@ module.exports = {
         icon: `src/assets/images/mid.png`, // This path is relative to the root of the site.
       },
     },
+
+    // Prismic
+    // See: https://github.com/angeloashmore/gatsby-source-prismic
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        // The name of your prismic.io repository. This is required.
+        repositoryName: 'mid-dummy',
+
+        // An API access token to your prismic.io repository. This is required.
+        // The token will be listed under "Permanent access tokens".
+        accessToken: 'MC5YUS1aWFJFQUFOd0FjLUxq.77-9CX3vv73vv71w77-977-9Y2jvv73vv73vv71277-977-9F21477-977-9K1MwRO-_ve-_vS8S77-9KE8',
+
+        // Set a link resolver function used to process links in your content.
+        // See: https://prismic.io/docs/javascript/query-the-api/link-resolving
+        linkResolver: ({ node, key, value }) => doc => {
+          if (doc.type === 'blog') return "/post/" + doc.uid;
+          return "/doc/" + doc.id;
+        },
+
+        // Set a list of links to fetch and be made available in your link
+        // resolver function.
+        // The fetchLinks option allows you to retrieve a specific content field from a linked document and add it to the document response object.
+        // See: https://prismic.io/docs/javascript/query-the-api/fetch-linked-document-fields
+        fetchLinks: [
+          // Your list of links
+        ],
+
+        // Set an HTML serializer function used to process formatted content.
+        // Fields with rich text formatting use this function to generate the
+        // correct HTML.
+        // This allows you to do things like adding custom classes to certain elements or adding a target element to all hyperlinks.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different HTML serializer logic for each field if necessary.
+        // See: https://prismic.io/docs/nodejs/beyond-the-api/html-serializer
+        htmlSerializer: () => {
+          // prismicHtmlSerializer
+        },
+
+        // Set a default language when fetching documents. The default value is
+        // '*' which will fetch all languages.
+        // See: https://prismic.io/docs/javascript/query-the-api/query-by-language
+        lang: '*',
+
+        // Set a function to determine if images are downloaded locally and made
+        // available for gatsby-transformer-sharp for use with gatsby-image.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different logic for each field if necessary.
+        // This defaults to always return true.
+        shouldNormalizeImage: ({ node, key, value }) => {
+          // Return true to normalize the image or false to skip.
+        },
+      },
+    },
+
     // Analytics using the new GTAG
     {
       resolve: `gatsby-plugin-gtag`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const _ = require('lodash')
 
-// You can delete this file if you're not using it
+const wrapper = promise =>
+  promise.then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    return result
+  })
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const postTemplate = require.resolve('./src/templates/blog.js')
+
+  const result = await wrapper(
+    graphql(`
+      {
+        allPrismicBlogs {
+          edges {
+            node {
+              id
+              uid
+            }
+          }
+        }
+      }
+    `)
+  )
+
+  const postsList = result.data.allPrismicBlogs.edges
+
+  // Double check that the post has a category assigned
+  postsList.forEach(edge => {
+    // The uid you assigned in Prismic is the slug!
+    createPage({
+      path: `/blogs/${edge.node.uid}`,
+      component: postTemplate,
+      context: {
+        // Pass the unique ID (uid) through context so the template can filter by it
+        uid: edge.node.uid,
+      },
+    })
+  })
+}
